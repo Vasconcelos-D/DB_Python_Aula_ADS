03b71bc3e6d5f825d524360c1387647011f0a35a
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-23 15:08:21 -0300

    Atualizando Tabela

--- script_insert_dinamic_parameter.py
+++ script_insert_dinamic_parameter.py
@@ -10,23 +10,18 @@ cursor = conexao.cursor()
 
 # Cria uma instância da classe Pessoa com os valores fornecidos:
 # CPF: 200000000, Nome: 'José', Data de nascimento: '1993-01-21', Usa óculos: False
-pessoa = Pessoa(200000000, 'José', '1993-01-21', False)
+pessoa = Pessoa(300000000, 'Silva', '1997-08-22', True)
 
 # O comando SQL que será executado. Esse comando insere valores na tabela 'Pessoa'.
 # Os campos esperados são: cpf, nome, data de nascimento e se a pessoa usa óculos.
-comando = '''
-INSERT INTO Pessoa (cpf, nome, nascimento, oculos)
-VALUES (:cpf, :nome, :data_nascimento, :usa_oculos);'''
+
+comando = '''INSERT INTO Pessoa VALUES (:cpf, :nome, :data_nascimento, :usa_oculos);'''
+
 # Aqui os valores estão sendo passados como parâmetros nomeados (:cpf, :nome, etc.)
 # Isso ajuda a evitar problemas de SQL injection e torna o código mais legível.
 
-# Executa o comando SQL, substituindo os parâmetros nomeados pelos valores do objeto 'pessoa'.
-cursor.execute(comando, {
-    "cpf": pessoa.cpf,  # Substitui o parâmetro ':cpf' pelo valor de 'pessoa.cpf'
-    "nome": pessoa.nome,  # Substitui ':nome' pelo valor de 'pessoa.nome'
-    "data_nascimento": pessoa.data_nascimento,  # Substitui ':data_nascimento' pelo valor de 'pessoa.data_nascimento'
-    "usa_oculos": pessoa.usa_oculos  # Substitui ':usa_oculos' pelo valor de 'pessoa.usa_oculos'
-})
+cursor.execute(comando, vars(pessoa))
+print(pessoa)
 
 # Confirma (efetiva) as mudanças no banco de dados. Sem isso, os dados não serão salvos.
 conexao.commit()
