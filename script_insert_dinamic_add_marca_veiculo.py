ab8cea948638a55579e37510e80f81e7c63e4f7d
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-23 22:54:11 -0300

    Corrigido sem erros !!

--- script_insert_dinamic_add_marca_veiculo.py
+++ script_insert_dinamic_add_marca_veiculo.py
@@ -1,14 +1,11 @@
-import sqlite3 as conector  # Importa a biblioteca sqlite3 e a renomeia como "conector"
-from modelo import Marca, Veiculo #Importando as Classes MARCA E VEICULO.
+import sqlite3 as conector  
+from modelo import Marca, Veiculo 
 
-# Abrindo uma conexão com o banco de dados SQLite.
-# Se o arquivo 'meu_banco.db' não existir, ele será criado.
 conexao = conector.connect("./meu_banco.db")
 conexao.execute("PRAGMA foreign_keys = on")
 
-# Cria um cursor para realizar operações no banco de dados.
-cursor = conexao.cursor()
 
+cursor = conexao.cursor()
 
 
 comando1 = '''INSERT INTO Marca (nome, sigla) VALUES  (:nome, :sigla);'''
@@ -21,8 +18,9 @@ marca2 = Marca(2, "Marca B", "MB")
 cursor.execute(comando1, vars(marca2))
 marca2.id = cursor.lastrowid
 
- # Inserção de dados na tabela Veiculo
-comando2 = '''INSERT INTO Veiculo VALUES (:placa, :ano, :cor, :motor, :proprietario, :marca);'''
+
+comando2 = '''INSERT INTO Veiculo (placa, ano, cor, motor, proprietario, marca)
+VALUES  (:placa, :ano, :cor, :motor, :proprietario, :marca);'''
 
 veiculo1 = Veiculo("AAA0001", 2001, "Prata", 1.0, 10000000099, marca1.id)
 veiculo2 = Veiculo("BAA0002", 2002, "Preto", 1.4, 10000000099, marca1.id)
@@ -34,12 +32,9 @@ cursor.execute(comando2, vars(veiculo2))
 cursor.execute(comando2, vars(veiculo3))
 cursor.execute(comando2, vars(veiculo4))
 
-
-# Confirma (efetiva) as mudanças no banco de dados. Sem isso, os dados não serão salvos.
 conexao.commit()
 
-# Fecha o cursor após a execução dos comandos SQL, liberando recursos.
+
 cursor.close()
 
-# Fecha a conexão com o banco de dados.
 conexao.close()
