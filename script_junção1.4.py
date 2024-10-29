40ff3d54583ebbf19bbad71db9838164e9e0b807
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-29 16:45:04 -0300

    ATUALIZANDO0

--- script_junção1.4.py
+++ script_junção1.4.py
@@ -1,23 +1,4 @@
-import sqlite3 as conector #Importando a biblioteca sqlite3 com o apelido conector
+import sqlite3 as conector 
 from modelo import Pessoa
-from s
-
-#Abrindo a conexão 
-conexao = conector.connect("./meu_banco.db") #Conectando com Banco de Dados
-cursor = conexao.cursor() #Criando o cursor
-
-comando = '''SELECT * FROM 
-                Veiculo JOIN Marca ON Marca.id = Veiculo.marca;''' #Comondo do Banco de Dados
-cursor.execute(comando) #Execução do comando
-
-registros = cursor.fetchall()
-
-for registro in registros:
-    print(registro)
-    marca = Marca(*registro[6:])
-    veiculo = Veiculo(*registro[:5], marca)
-    print("Placa:", veiculo.placa, ", Marca:", veiculo.marca.nome)
-    
-cursor.close()
-conexao.close()
+from script_junção1.3
 
